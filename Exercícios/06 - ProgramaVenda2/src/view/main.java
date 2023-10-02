f30f3ab1a2e285fab78a2780edf456a216ff5bcb
AryaneMachado <aryanecassimiro0309@gmail.com>
2023-10-02 08:48:42 -0300

    02/10 - Finalizando Programa Venda 2

--- Exercícios/06 - ProgramaVenda2/src/view/main.java
+++ Exercícios/06 - ProgramaVenda2/src/view/main.java
@@ -10,30 +10,32 @@ public class main {
 		
 		Product arroz = new Product("Arroz Pileco T1", 15);
 		Product feijao = new Product("Feijão Malícia", 20);
+		Product sal = new Product("Sal Cisne", 3);
 		
-		
-		Sale sale = new Sale();
 		SaleItem siArroz = new SaleItem(arroz, 3);
 		SaleItem siFeijao = new SaleItem (feijao, 2);
+		SaleItem siSal = new SaleItem(sal, 2);
+		
+		Sale sale = new Sale();
+		sale.addSaleItem(siArroz);
+		sale.addSaleItem(siFeijao);
+		sale.addSaleItem(siSal);
+		
 		
 		System.out.println(String.format("Venda - #1234 - %s", 
 				sale.getDate().toLocaleString()));
 		
 		for(SaleItem saleItem : sale.getSaleItems()) {
-			System.out.println(saleItem.getProduct().getDescription() + " - ");
-			System.out.println(saleItem.getAmount() + " - ");
-			System.out.println("R$" + saleItem.getProduct().getPrice() + " - ");
-			System.out.println("R$" + saleItem.getSaleItemSubTotal() + "\n");
+			System.out.print(saleItem.getProduct().getDescription() + " - ");
+			System.out.print(saleItem.getAmount() + " - ");
+			System.out.print("R$" + saleItem.getProduct().getPrice() + " - ");
+			System.out.print("R$" + saleItem.getSaleItemSubTotal() + "\n");
 			
 		}
 		
-		System.out.println("Total: R$"+sale.getTotal());
+		System.out.print("Total: R$" + sale.getTotal());
 		
-		
-		
-	
-
-}
+	}
 
 
 }
