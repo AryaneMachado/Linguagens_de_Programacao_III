dc21b973a5e2eda38f2022899d8b9bba48cac1d0
AryaneMachado <aryanecassimiro0309@gmail.com>
2023-12-05 10:46:11 -0300

    Ary2 - 05/12 - Importações concluídas´com Apache

---
+++ facebook/src/main/java/controller/UsersController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = {""}) // Definição de rotas
+public class UsersController extends HttpServlet {
+	
+	
+
+}
